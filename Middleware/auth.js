75e21ab285c171504b06c2c3abef54dd5700f248
Milo Canseco <canseco3rd@gmail.com>
2026-03-09 02:14:34 -0700

    Removed extra next() syntax.

--- Middleware/auth.js
+++ Middleware/auth.js
@@ -16,8 +16,6 @@ export const authenticateToken = (req, res, next) => {
     req.user = decoded;
     console.log("Decoded token:", decoded);
     next();
-
-    next();
   } catch (error) {
     return res.status(403).json({ error: "Invalid or expired token" });
   }
